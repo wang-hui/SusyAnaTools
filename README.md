6a0cde7774dcb7cefe7e075d7ff3b44791d6b13c
Zhenbin Wu <benwu@fnal.gov>
2015-07-10 13:38:26 -0500

    Update README for this branch

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 CommonTools
 
   This repository holds tools which are common to all sub-analysis groups.  
+
+
+## Code Recipe for Fat Top Tagger
+
+```
+cd CMSSW_7_3_0/src/
+cmsenv
+git cms-merge-topic gkasieczka:htt-v2 # V2 HEP
+git clone git@github.com:gkasieczka/ShowerDeconstruction.git ## Private code
+git clone -b TestMiniAOD git@github.com:susy2015/recipeAUX.git
+git clone -b align_leptDef_better_search_bins git@github.com:susy2015/SusyAnaTools.git
+```
+
+### PUPPI
+```
+git cms-addpkg CommonTools/PileupAlgos
+git remote add nhan-remote https://github.com/nhanvtran/cmssw.git
+git fetch nhan-remote puppi-bugfix-for-miniaod
+git cherry-pick 0585bf21ae098f14f144b9a3d361178e6cc830e6
+scram b
+```
+https://twiki.cern.ch/twiki/bin/viewauth/CMS/PUPPI
+
+
+### CMS Top Tagger
+https://twiki.cern.ch/twiki/bin/view/CMSPublic/WorkBookMiniAOD2014#Jets
+
+### HEP V2 Top Tagger
+https://twiki.cern.ch/twiki/bin/viewauth/CMS/JME14004Organization#HEPTopTagge
+
+### Soft Drop
+git clone https://github.com/cms-jet/JetToolbox JMEAnalysis/JetToolbox -b jetToolbox_73X
+
+### Shower Decontruction
+Code remain private
--- SkimsAUX/workdir/prod/crab_example/crab3Config.py
+++ SkimsAUX/workdir/prod/crab_example/crab3Config.py
@@ -3,11 +3,12 @@ config = Configuration()
 
 config.section_("General")
 config.General.requestName = 'PHYS14_PU20bx25_TTJets'
+config.General.workArea = 'Test1'
+config.General.transferLogs = True
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'treeMaker_stopRA2.py'
-config.JobType.allowNonProductionCMSSW = False
 
 config.section_("Data")
 config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
@@ -18,9 +19,9 @@ config.Data.publication = False
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 config.Data.publishDataName = 'PHYS14_PU20bx25_PHYS14_25_V1-FLAT'
 #Use your own username instead of the "lhx". Keep branch tag in the directory name, e.g., PHYS14_720_Dec23_2014.
-config.Data.outLFN = '/store/group/lpcsusyhad/PHYS14_720_Mar14_2014_v2/lhx/PU20bx25_TTJets_MSDecaysCKM_madgraph-tauola/'
+config.Data.outLFNDirBase = '/store/user/benwu/JetMET_TP/MET/First/'
 
-config.Data.ignoreLocality = False
+config.Data.ignoreLocality = True
 
 config.section_("Site")
 config.Site.storageSite = 'T3_US_FNALLPC'
